64a450ac07e1ab3245da8567141c1066a43668a1
gabkhanfig <gabkhanfig@gmail.com>
2023-06-29 15:21:47 -0400

    Enemy Knocked Back

--- Assets/Enemies/Enemy.cs
+++ Assets/Enemies/Enemy.cs
@@ -1,25 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class Enemy : MonoBehaviour, IEnemy
 {
-    [SerializeField]
     private Health healthComponent;
-
-    public Health GetHealthComponent() {return healthComponent;}
-
-    //public Health healthComponent {get; private set;}
-
+    private KnockbackImpulse knockbackComponent;
+    private Rigidbody2D rb2d;
+    
     // Start is called before the first frame update
     void Start()
     {
-        healthComponent = gameObject.AddComponent(typeof(Health)) as Health;
+        healthComponent = GetComponent<Health>();
+        Debug.Assert(healthComponent != null, "Enemy must have a health component");
+        rb2d = GetComponent<Rigidbody2D>();
+        Debug.Assert(rb2d != null, "Enemy must have a rigidbody2d component");
+        knockbackComponent = GetComponent<KnockbackImpulse>(); // Can be null
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
+
+#region IEnemy 
+
+    public Health GetHealthComponent() {return healthComponent;}
+
+    public void OnDamageEvent(int damageAmount, Vector2 damageDirection, bool isDead) {
+        Debug.LogFormat("{0} health remaining", healthComponent.currentHealth);
+        knockbackComponent?.Knockback(damageDirection);
+    }
+
+    public void OnHealingEvent(int healingAmount) {
+
+    }
+
+#endregion IEnemy
 }
--- Assets/Enemies/IEnemy.cs
+++ Assets/Enemies/IEnemy.cs
@@ -5,4 +5,6 @@ using UnityEngine;
 public interface IEnemy
 {
         Health GetHealthComponent();
+        void OnDamageEvent(int damageAmount, Vector2 damageDirection, bool isDead);
+        void OnHealingEvent(int healingAmount);
 }
